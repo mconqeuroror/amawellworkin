bd5d9657ec8b0eb192565ac787b6b6530e53dc6f
mconqeuroror <mconqueror17@gmail.com>
2025-07-14 10:29:45 +0200

    Update .puppeteerrc.cjs

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -1,4 +1,4 @@
 const { join } = require('path');
 module.exports = {
-  cacheDirectory: process.env.NODE_ENV === 'production' ? '/app/.cache/puppeteer' : join(__dirname, '.cache', 'puppeteer'),
+  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
 };
